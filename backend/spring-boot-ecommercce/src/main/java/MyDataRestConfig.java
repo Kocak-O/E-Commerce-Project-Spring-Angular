9163ab9c182e1695e2443ff4b7fe5104cb9d03e9
OsmanKocak17 <osman.kocak0098@gmail.com>
2025-11-16 19:43:31 +0100

    added unsupported HttpMethods (for Security aspect)

---
+++ backend/spring-boot-ecommercce/src/main/java/MyDataRestConfig.java
@@ -0,0 +1,18 @@
+import kocak.springbootecommercce.entity.Product;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+    }
+}
