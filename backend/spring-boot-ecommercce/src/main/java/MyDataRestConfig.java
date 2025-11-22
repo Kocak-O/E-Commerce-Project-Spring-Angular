21745e96f670bbf458fffab4e0c6414f14b05fcc
OsmanKocak17 <osman.kocak0098@gmail.com>
2025-11-22 18:55:05 +0100

    created angular component and service for Product

--- backend/spring-boot-ecommercce/src/main/java/MyDataRestConfig.java
+++ backend/spring-boot-ecommercce/src/main/java/MyDataRestConfig.java
@@ -1,18 +1,30 @@
 import kocak.springbootecommercce.entity.Product;
+import kocak.springbootecommercce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 
+@Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+    // disable HTTP Methods for Product and ProductCategory: POST, PUT and DELETE
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
-        HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
 
         config.getExposureConfiguration()
                 .forDomainType(Product.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
     }
+
 }
--- frontend/angular-ecommerce/src/app/app.component.ts
+++ frontend/angular-ecommerce/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SalesPersonListComponent } from './sales-person-list/sales-person-list.component';
-import { SalesPerson } from './sales-person-list/sales-person';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SalesPersonListComponent],
+  imports: [RouterOutlet, ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+  title = 'angular-ecommerce';
 }
